fdaf5f8662ff1c30162ac776f859abb810d75a74
manuel <manuelperez.0000@gmail.com>
2023-04-23 20:05:40 -0400

    cross-env installed

--- src/routes/serverRoute.js
+++ src/routes/serverRoute.js
@@ -1,4 +1,6 @@
-//dev
-//const serverRoute = "http://localhost:3000/"
-const serverRoute = "https://vimo-app.vercel.app/"
+const environment = process.env.NEXT_PUBLIC_ENV;
+const LOCAL = "http://localhost:3000/"
+const PROD =  "https://vimo-app.vercel.app"
+const serverRoute = environment === 'dev' ? LOCAL : PROD;
 export default serverRoute
+
